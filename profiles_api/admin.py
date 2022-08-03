b48ceacc39d56f31dd1fa8963f72ef54db96bc6d
Kılıçarslan <xswordlionx@gmail.com>
2022-08-03 15:34:04 +0300

    Added user profile feed API

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -5,6 +5,7 @@ from profiles_api import models
 
 
 admin.site.register(models.UserProfile)
+admin.site.register(models.ProfileFeedItem)
 
 
 # Register your models here.
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,7 +7,7 @@ from profiles_api import views
 router = DefaultRouter()
 router.register('project-viewset', views.ProjectViewSet, base_name='project-viewset')
 router.register('profile', views.UserProfileViewSet)
-
+router.register('feed', views.UserProfileFeedViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
